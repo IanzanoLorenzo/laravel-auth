f41dee39d37d0653070b85a42c16a9e947302a44
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-08-23 22:36:12 +0200

    modifica fade del modale

--- resources/views/admin/project/partials/modals.blade.php
+++ resources/views/admin/project/partials/modals.blade.php
@@ -1,4 +1,4 @@
-<div id="modale" class="modal" tabindex="-1">
+<div id="modale" class="modal fade" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
